e740533f42eb784dff2e5211425d9856ca17f23a
Zach Nusbaum <nusbaumz@msu.edu>
2016-08-13 20:48:46 -0700

    define from_omniauth on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: [:github]
 
+  def self.from_omniauth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
   private
 
   def set_token
